4743945f84e7a96984dff1bd05148efc58ff97d3
zkx <kaixuanzkx@163.com>
2018-10-31 11:21:36 +0800

    测试

---
+++ src/test/java/com/zkx/spingboot/TestControllerTest.java
@@ -0,0 +1,22 @@
+package com.zkx.spingboot;
+
+
+import com.zkx.spingboot.controller.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@SpringBootTest(classes = WebApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestControllerTest {
+    @Autowired
+    private HomeController homeController;
+
+    @Test
+    public void testZkx() {
+        System.out.println(homeController.hello());
+    }
+}
